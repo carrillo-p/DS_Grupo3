c6c4126c813b43292ba4382709d8d8e7f7e5ef98
Lisy29 <damaliamzn@gmail.com>
2024-10-14 13:55:22 +0200

    delete gui files

--- aux_functions.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_data():
-    # Función para cargar los datos
-    return pd.read_csv('src/Data/stroke_woe_smote.csv')
-
-# Agrega más funciones auxiliares según sea necesario
